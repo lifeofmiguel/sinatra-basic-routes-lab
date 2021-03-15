9f2a8c70839b6698f9759b60477f8603cb45e80b
Miguel Angel Morales <mmorales21500@live.com>
2021-03-15 17:50:46 -0400

    commit tree 06c00b5eaf6e5387e4898b272d3e85c685ca3aff

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Miguel"
+    end  
+    get '/hometown' do 
+        "My hometown is Baltimore"
+    end
+    
+      get '/favorite-song' do
+        "My favorite song is Cara a la Muerte"
+    end     
 end
